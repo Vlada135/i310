8d0c2d8bfe63ce306f4715aaed77fc9d649b94cf
Vlada135 <vladislavababul@gmail.com>
2023-09-12 19:33:41 +0300

    Initial commit

---
+++ i310/i310/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i310
+//
+//  Created by Владислава on 12.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
